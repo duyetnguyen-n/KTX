5924d4b0cdaf7a957a7489de7537b10a94e52236
nhuha2810 <nhuha2810@gmail.com>
2023-12-15 14:43:11 +0700

    ha gui

---
+++ back_end/add-sv.php
@@ -0,0 +1,117 @@
+
+
+
+
+
+<?php
+    session_start();
+
+    include '../config/database.php'; // Import file database.php
+
+    // Khởi tạo đối tượng Database
+    $db = new Database();
+
+    if (empty($_SESSION['username'])) {
+    header("location: dang_nhap.html");
+    exit();
+    } else {
+    include 'header.php';
+    include 'side-bar.php';
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $ma_sinh_vien = $_POST["ma_sinh_vien"];
+        $ten_tai_khoan = $_POST["ten_nguoi_dung"];
+        $ho_va_ten = $_POST["ho_ten"];
+        $mat_khau = $_POST["mat_khau"];
+        
+        $email = $_POST["email"];
+        $so_dien_thoai = $_POST["so_dien_thoai"];
+
+    // Xử lý ảnh đại diện
+    $anh_dai_dien = $_FILES["anh_dai_dien"]["name"];
+    $anh_dai_dien_tmp = $_FILES["anh_dai_dien"]["tmp_name"];
+    move_uploaded_file($anh_dai_dien_tmp, "../assets/img/$anh_dai_dien");
+
+
+        // Thực hiện câu lệnh SQL thêm phòng
+        $sql = "INSERT INTO client (ma_sinh_vien, ten_nguoi_dung, mat_khau, ho_ten, email, so_dien_thoai, trang_thai, anh_dai_dien)
+            VALUES ('$ma_sinh_vien', '$ten_tai_khoan', '$mat_khau', '$ho_va_ten', '$email', '$so_dien_thoai', 'sẵn sàng', '$anh_dai_dien')";
+
+    if ($db->insert($sql)) {
+        echo '<script>alert("Thêm sinh viên thành công!"); window.location.href = "list-sv.php";</script>';
+
+        exit(); // Đảm bảo dừng thực thi mã nguồn sau khi chuyển hướng
+    } else {
+        echo '<script>alert("Lỗi khi thêm sinh viên ' . $db->getError() . '");</script>';    }
+    }
+    ?>
+    <div class="col-md-9">
+        <div class="col-12 container-content">
+            <div class="header-content pb-3">
+            <div class="title-content">
+                    <h3 class="m-0 strong">Thêm sinh viên</h3>
+                    <div class="span">Quản lý sinh viên</div>
+                </div>
+            </div>
+            <div class="content-add-room col-12">
+                <form action="add-sv.php" method="post" enctype="multipart/form-data">
+                    <div class="row">
+                    <div class="col-12 col-md-6 p-2">
+                            <label for="ma_sinh_vien">Mã sinh viên</label>
+                            <input type="text" id="ma_sinh_vien" name="ma_sinh_vien" class="form-control" required>
+                        </div>
+                        <div class="col-12 p-2">
+                            <label for="ten_nguoi_dung">Tên người dùng</label>
+                            <input type="text" id="ten_nguoi_dung" name="ten_nguoi_dung" class="form-control"required>
+                        </div>
+                        <div class="col-12 p-2">
+                            <label for="ho_ten">Họ tên</label>
+                            <input type="text" id="ho_ten" name="ho_ten" class="form-control"required>
+                        </div>
+                        <div class="col-12 col-md-4 p-2">
+                            <label for="mat_khau">Mật khẩu</label>
+                            <input type="text" id="mat_khau" name="mat_khau" class="form-control" required>
+                        </div>
+                        <div class="col-12 col-md-6 p-2">
+                            <label for="email">Email</label>
+                            <input type="text" id="email" name="email" class="form-control" required>
+                        </div>
+                        
+                        <div class="col-12 col-md-4 p-2">
+                            <label for="so_dien_thoai">Số điện thoại</label>
+                            <input type="text" id="so_dien_thoai" name="so_dien_thoai" class="form-control" required>
+                        </div>
+                        <div class="col-12 col-md-4 p-2">
+                            <label for="trang_thai">Trạng thái</label>
+                            <input type="text" id="trang_thai" name="trang_thai" class="form-control">
+                        </div>
+                       
+                        <div class="col-12 p-2">
+                            <label>Ảnh đại diện:</label>
+                            <div class="flex-container">
+                                <!-- Trong HTML -->
+                                <div class="drag-area-1 drag-area w-full rounded-2xl border-2 border-dashed items-center">
+                                    <header class="text-lg text-black-50">Kéo và Thả để Tải ảnh lên</header>
+                                    <span class="my-3 text-sm text-black-50">Hoặc</span>
+                                    <div class="but-add-image">Chọn ảnh</div>
+                                    <input type="file" name="anh_dai_dien" hidden>
+                                    <div class="remove-image" onclick="removeImage(this)">-</div>
+                                </div>
+                            </div>
+                        </div>
+                       
+                        <div class="col-12">
+                            <input class="but-add mr-2" type="submit" value="Thêm">
+                            <input class="but-cancel" type="button" value="Hủy">
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <script src="../assets/js/area-image.js"></script>
+        
+    </body>
+    </html>
+<?php } ?>
+
---
+++ back_end/edit-sv.php
@@ -0,0 +1,159 @@
+<?php
+session_start();
+
+include '../config/database.php'; // Import file database.php
+
+// Khởi tạo đối tượng Database
+$db = new Database();
+
+if (empty($_SESSION['username'])) {
+    header("location: dang_nhap.html");
+    exit();
+} else {
+    include 'header.php';
+    include 'side-bar.php';
+    $id = $_GET['id'];
+
+    // Lấy dữ liệu từ cơ sở dữ liệu
+    $sql = "SELECT * FROM client WHERE id_client = $id";
+    $result = $db->select($sql);
+    if ($result->num_rows == 1) {
+        $row = $result->fetch_assoc();
+       
+        $ma_sinh_vien = $row["ma_sinh_vien"];
+        $ten_nguoi_dung = $row["ten_nguoi_dung"];
+        $ho_ten = $row["ho_ten"];
+        $mat_khau = $row["mat_khau"];
+        
+        $email = $row["email"];
+        $so_dien_thoai = $row["so_dien_thoai"];
+        $trang_thai= $row["trang_thai"];
+        $anh_dai_dien = $row['anh_dai_dien'];
+       
+    } else {
+        echo "Không tìm thấy dữ liệu.";
+    }
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $newma_sinh_vien = $_POST["ma_sinh_vien"];
+        $newten_nguoi_dung= $_POST["ten_nguoi_dung"];
+        $newho_ten = $_POST["ho_ten"];
+        $newmat_khau = $_POST["mat_khau"];
+        $newemail = $_POST["email"];
+        $newso_dien_thoai = $_POST["so_dien_thoai"];
+        $newtrang_thai= $_POST["trang_thai"];
+
+        // Xử lý ảnh đại diện mới
+        if ($_FILES["anh_dai_dien"]["name"] != "") {
+            $newanh_dai_dien = $_FILES["anh_dai_dien"]["name"];
+            $newanh_dai_dien_tmp = $_FILES["anh_dai_dien"]["tmp_name"];
+            move_uploaded_file($newanh_dai_dien_tmp, "../assets/img/$newanh_dai_dien");
+        } else {
+            // Giữ nguyên ảnh đại diện cũ
+            $newanh_dai_dien = $anh_dai_dien;
+        }
+
+       
+
+        // ... (your existing code)
+
+        // Thực hiện câu lệnh SQL sửa phòng
+        $sql = "UPDATE client SET 
+                    ma_sinh_vien='$newma_sinh_vien',
+                    ten_nguoi_dung='$newten_nguoi_dung', 
+                    ho_ten='$newho_ten', 
+                    mat_khau='$newmat_khau', 
+                    email='$newemail', 
+                    so_dien_thoai='$newso_dien_thoai', 
+                    trang_thai='$newtrang_thai', 
+                    anh_dai_dien='$newanh_dai_dien'
+                    
+                    WHERE id_client=$id";
+
+if ($db->update($sql)) {
+    echo '<script>alert("Sửa sinh viên thành công!"); window.location.href = "list-sv.php";</script>';
+    exit(); // Đảm bảo dừng thực thi mã nguồn sau khi chuyển hướng
+} else {
+    echo '<script>alert("Lỗi khi sửa sinh viên ' . $db->getError() . '");</script>';
+}
+}?>
+
+
+<div class="col-md-9">
+    <div class="col-12 container-content">
+        <div class="header-content pb-3">
+            <div class="title-content">
+                <h3 class="m-0 strong">Sửa sinh viên</h3>
+                <div class="span">Quản lý sinh viên</div>
+            </div>
+        </div>
+        <div class="content-add-room col-12">
+            <form action="edit-sv.php?id=<?php echo $id; ?>" method="post" enctype="multipart/form-data">
+                <!-- ... (your existing form fields) -->
+                <div class="row">
+                    <div class="col-12 col-md-6 p-2">
+                        <label for="ma_sinh_vien">Mã sinh viên</label>
+                        <input type="text" id="ma_sinh_vien" name="ma_sinh_vien" class="form-control" value="<?php echo $ma_sinh_vien; ?>" required>
+                    </div>
+                    <div class="col-12 col-md-6 p-2">
+                        <label for="ten_nguoi_dung">Tên người dùng</label>
+                        <input type="text" id="ten_nguoi_dung" name="ten_nguoi_dung" class="form-control" value="<?php echo $ten_nguoi_dung; ?>" required>
+                    </div>
+                    <div class="col-12 col-md-4 p-2">
+                        <label for="ho_ten">Họ tên</label>
+                        <input type="text" id="ho_ten" name="ho_ten" class="form-control" value="<?php echo $ho_ten; ?>" required>
+                    </div>
+                    <div class="col-12 col-md-4 p-2">
+                        <label for="mat_khau">Mật khẩu</label>
+                        <input type="text" id="mat_khau" name="mat_khau" class="form-control" value="<?php echo $mat_khau; ?>" required>
+                    </div>
+                    <div class="col-12 p-2">
+                        <label for="email">Email</label>
+                        <input type="text" id="email" name="email" class="form-control" value ="<?php echo $email; ?>" required>
+                        
+                    </div>
+                    <div class="col-12 p-2">
+                        <label for="so_dien_thoai">Số điện thoại</label>
+                        <input type="text" id="so_dien_thoai" name="so_dien_thoai" class="form-control" value ="<?php echo $so_dien_thoai; ?>" required>
+                    </div>
+                    <div class="col-12 col-md-4 p-2">
+                        <label for="trang_thai">Trạng thái:</label>
+                        <input type="text" id="trang_thai" name="trang_thai" class="form-control" value="<?php echo $trang_thai; ?>" required>
+                    </div>
+                    
+                   
+                    <!-- Images 1 area -->
+                    <div class="col-12 p-2">
+                        <label>Ảnh đại diện:</label>
+                        <div class="flex-container">
+                            <div class="drag-area-1 drag-area w-full rounded-2xl border-2 border-dashed items-center">
+                                <header class="text-lg text-black-50">Kéo và Thả để Tải ảnh lên</header>
+                                <span class="my-3 text-sm text-black-50">Hoặc</span>
+                                <div class="but-add-image">Chọn ảnh</div>
+                                <input type="file" name="anh_dai_dien" hidden>
+                                <input type="hidden" name="anh_dai_dien_old" value="<?php echo $anh_dai_dien; ?>">
+                                <input type="hidden" id="anh_dai_dien_preview" value="<?php echo $anh_dai_dien; ?>">
+                                <div class="image-container">
+                                    <img id="anh_dai_dien_preview_img" src="../assets/img/<?php echo $anh_dai_dien; ?>" alt="Ảnh đại diện" class="uploaded-image img-thumbnail img-fluid img-responsive"/>
+                                    <div class="remove-image" onclick="removeImage(this)">-</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    
+
+                    <div class="col-12">
+                        <input class="but-add mr-2" type="submit" value="Cập nhật">
+                        <input class="but-cancel" type="button" value="Hủy">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<script src="../assets/js/area-image.js"></script>
+   
+</body>
+</html>
+<?php } ?>
